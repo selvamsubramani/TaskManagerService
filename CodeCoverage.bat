331b6e736880a5796cdcc9bcb7ce6df2d3ff0a80
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-09 05:26:32 -0700

    Fixed the package folder issue in batch file

--- CodeCoverage.bat
+++ CodeCoverage.bat
@@ -2,6 +2,6 @@
 
 rem CodeCoverage using Open Cover
 
-%~dp0packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe" -targetargs:"/noisolation /testcontainer:TaskManager.Test\bin\Release\TaskManager.Test.dll" -register:user -output:TestResult.xml -excludebyattribute:*.ExcludeFromCoverage* -mergebyhash -skipautoprops
+"%~dp0packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe" -targetargs:"/noisolation /testcontainer:TaskManager.Test\bin\Release\TaskManager.Test.dll" -register:user -output:TestResult.xml -excludebyattribute:*.ExcludeFromCoverage* -mergebyhash -skipautoprops
 
 exit
