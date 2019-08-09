c958172974c52ad42ee041fce79fcd85f50125c4
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-09 05:15:36 -0700

    CodeCoverage batch file is updated

--- CodeCoverage.bat
+++ CodeCoverage.bat
@@ -1,2 +1,2 @@
-﻿
-"packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe" -targetargs:"/noisolation /testcontainer:TaskManager.Test\bin\**\TaskManager.Test.dll" -register:user -output:TestResult.xml
+﻿rem CodeCoverage using Open Cover
+~dp0packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe" -targetargs:"/noisolation /testcontainer:TaskManager.Test\bin\Release\TaskManager.Test.dll" -register:user -output:TestResult.xml -excludebyattribute:*.ExcludeFromCoverage*
