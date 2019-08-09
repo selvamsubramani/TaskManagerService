5017ce3e40361a24934bdf391176c8a2e757943d
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-09 05:39:47 -0700

    Code Coverage Report batch file is updated

--- CodeCoverageReport.bat
+++ CodeCoverageReport.bat
@@ -1,2 +1,7 @@
-﻿
-"packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe" -reports:TaskManager.Test\bin\**\TestResult.xml -targetdir:"CodeCoverage-OpenCover"
+﻿@echo off
+
+rem CodeCoverageReport using ReportGenerator
+
+"%~dp0packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe" -reports:"TaskManager.Test\bin\Release\TestResult.xml" -targetdir:"CodeCoverage-OpenCover"
+
+exit
